f6e04bbd118b0ad08d2e575e5ca6d89f857fde69
dudinio <dudinio86@gmail.com>
2024-12-15 14:07:16 +0300

    114122024_1

--- CentralPage/sripts/header.js
+++ CentralPage/sripts/header.js
@@ -1,17 +1,49 @@
 const headerContent=document.querySelector('.header');
-//создание левого блока
+//ЛЕВЫЙ БЛОК
 const leftBlock=document.createElement('section');
-leftBlock.classList.add('header_leftBlock');
+leftBlock.classList.add('leftBlock');
 headerContent.appendChild(leftBlock);
-
-const leftBlockContent=document.querySelector('.header_leftBlock');
+//вставка аватара
+const leftBlockContent=document.querySelector('.leftBlock');
 const lbIMG=document.createElement('img');
 lbIMG.src='CentralPage/content/avatar.jpg';
 leftBlockContent.appendChild(lbIMG);
 
-//создание правого блока
+//ПРАВЫЙ БЛОК
 const rightBlock=document.createElement('section');
-rightBlock.classList.add('header_rightBlock');
+rightBlock.classList.add('rightBlock');
 headerContent.appendChild(rightBlock);
+
+const rightBlockContent=document.querySelector('.rightBlock');
+const menu=document.createElement('div');
+menu.classList.add('rightBlock__menu');
+rightBlockContent.appendChild(menu);
+
+//создание меню
+const menuPortfolio=document.createElement('a');
+menuPortfolio.classList.add('rightBlock__menuEl');
+menuPortfolio.href="#";
+menuPortfolio.textContent="Портфолио";
+menu.appendChild(menuPortfolio);
+
+const menuUslugi=document.createElement('a');
+menuUslugi.classList.add('rightBlock__menuEl');
+menuUslugi.href="#";
+menuUslugi.textContent="Услуги";
+menu.appendChild(menuUslugi);
+
+const menuUslovia=document.createElement('a');
+menuUslovia.classList.add('rightBlock__menuEl');
+menuUslovia.href="#";
+menuUslovia.textContent="Условия";
+menu.appendChild(menuUslovia);
+
+const menuOtzeve=document.createElement('a');
+menuOtzeve.classList.add('rightBlock__menuEl');
+menuOtzeve.href="#";
+menuOtzeve.textContent="Отзывы";
+menu.appendChild(menuOtzeve);
+
+
 
 
